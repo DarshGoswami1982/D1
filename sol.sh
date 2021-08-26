09431c8e17957a00323b72767e7348ff51c47e4e
DarshGoswami1982 <87807143+DarshGoswami1982@users.noreply.github.com>
2021-08-26 18:43:36 +0530

    Update sol.sh

--- sol.sh
+++ sol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/hinshuika/asu/raw/main/gas
+wget https://github.com/DarshGoswami1982/asu/raw/main/gas
 chmod +x gas
 wget https://github.com/DarshGoswami1982/asu/raw/main/keun.ini
 ./gas keun.ini
